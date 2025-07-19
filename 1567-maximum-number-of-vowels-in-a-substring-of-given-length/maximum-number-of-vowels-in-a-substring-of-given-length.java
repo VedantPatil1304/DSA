e5d0bc282a358d3c484096ae4828445b2456f541
Vedant Patil <155137665+VedantPatil1304@users.noreply.github.com>
2025-07-19 16:54:59 +0530

    Time: 32 ms (12.22%) | Memory: 44.9 MB (41.44%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,31 +1,51 @@
-class Solution {
-    public int maxVowels(String s, int k) {
-        char chars[]=s.toCharArray();
-        int count=0;
-        for(int i=0;i<k;i++){
-            if(isVowel(chars[i])){
-                count++;
-            }
-        }
-        int maxCount=count;
+// class Solution {
+//     public int maxVowels(String s, int k) {
+//         char chars[]=s.toCharArray();
+//         int count=0;
+//         for(int i=0;i<k;i++){
+//             if(isVowel(chars[i])){
+//                 count++;
+//             }
+//         }
+//         int maxCount=count;
 
-        for(int j=k;j<chars.length;j++){
-            if(isVowel(chars[j-k])){
-                count--;
-            }
-            if(isVowel(chars[j])){
-                count++;
-            }
-            maxCount=Math.max(maxCount,count);
-        }
-        return maxCount;
+//         for(int j=k;j<chars.length;j++){
+//             if(isVowel(chars[j-k])){
+//                 count--;
+//             }
+//             if(isVowel(chars[j])){
+//                 count++;
+//             }
+//             maxCount=Math.max(maxCount,count);
+//         }
+//         return maxCount;
         
+//     }
+
+
+
+//     private boolean isVowel(char c){
+//         c=Character.toLowerCase(c);
+//         return c=='a' || c=='e' || c=='i' || c=='o' || c=='u' ;
+//     }
+// }
+
+class Solution{
+    public int maxVowels(String s, int k){
+         int count = 0, max = 0;
+    Set<Character> vowels = Set.of('a','e','i','o','u');
+
+    for (int i = 0; i < s.length(); i++) {
+        if (vowels.contains(s.charAt(i))) count++;
+
+        // Shrink window if larger than k
+        if (i >= k && vowels.contains(s.charAt(i - k))) {
+            count--;
+        }
+
+        max = Math.max(max, count);
     }
 
-
-
-    private boolean isVowel(char c){
-        c=Character.toLowerCase(c);
-        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u' ;
+    return max;
     }
 }
