57660b751bdff002ffff12489621502c7bdf44b0
Vedant Patil <155137665+VedantPatil1304@users.noreply.github.com>
2025-07-22 08:08:52 +0530

    Time: 1 ms (51.76%) | Memory: 45.3 MB (18.21%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -1,22 +1,43 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int arr[]=new int[nums.length];
-        List<Integer> list=new ArrayList<>();
-        for(int i:nums){
-            if(i%2==0){
-                list.add(i);
-            }
-        }
-        for(int i:nums){
-            if(i%2!=0){
-                list.add(i);
-            }
-        }
+        // int arr[]=new int[nums.length];
+        // List<Integer> list=new ArrayList<>();
+        // for(int i:nums){
+        //     if(i%2==0){
+        //         list.add(i);
+        //     }
+        // }
+        // for(int i:nums){
+        //     if(i%2!=0){
+        //         list.add(i);
+        //     }
+        // }
 
-        for(int i=0;i<nums.length;i++){
-            arr[i]=list.get(i);
-        }
-        return arr;
+        // for(int i=0;i<nums.length;i++){
+        //     arr[i]=list.get(i);
+        // }
+        // return arr;
         
+
+        //optimised 2 ptr approach
+
+        int l=0, r=nums.length-1;
+        while(l<r){
+            if(nums[l]%2==0){
+                l++;
+            }
+            else if(nums[r]%2!=0){
+                r--;
+            }
+            else{
+                int temp=nums[l];
+                nums[l]=nums[r];
+                nums[r]=temp;
+                l++;
+                r--;
+            }
+
+        }
+        return nums;
     }
 }
