4de2e6c4bec66fc980f5026d4025aa939a7c1201
Vedant Patil <155137665+VedantPatil1304@users.noreply.github.com>
2025-08-11 11:58:06 +0530

    Time: 26 ms (35.97%) | Memory: 45.8 MB (30.69%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.java
+++ 402-remove-k-digits/remove-k-digits.java
@@ -2,37 +2,36 @@ class Solution {
     public String removeKdigits(String num, int k) {
         Stack<Character> st=new Stack<>();
 
-        for(char c:num.toCharArray()){
+        for(char c : num.toCharArray()){
             while(k>0 && !st.isEmpty() && st.peek()>c){
                 st.pop();
                 k--;
             }
             st.push(c);
         }
-        //
 
-        //edge case like "12345" where there is inc order and thus we cant remove any elements 
-        // in this type of cases we will remove k digits from the last
-
-        //this means even after complete for loop k is still greater than zero
+        //edge case 12345
         while(k>0 && !st.isEmpty()){
             st.pop();
             k--;
-        }
+        }       
 
-        //building result 
+        //building result
         StringBuilder sb=new StringBuilder();
 
         while(!st.isEmpty()){
             sb.append(st.pop());
-        }
+        } 
         sb.reverse();
 
-        //removing leading zeroes 
+        //removing leading zeroes
         while(sb.length()>0 && sb.charAt(0)=='0'){
             sb.deleteCharAt(0);
         }
 
-        return sb.length()==0? "0" : sb.toString();
+        if(sb.length()==0){
+            return "0";
+        }
+        return sb.toString();
     }
 }
